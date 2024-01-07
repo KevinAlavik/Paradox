d6ba1214bb10c56f5242a025688c8e80285b64d7
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2024-01-07 15:27:22 +0100

    sadasd

--- src/system/drivers/rtc.c
+++ src/system/drivers/rtc.c
@@ -1,2 +1,9 @@
 #include <system/drivers/rtc.h>
+#include <serial/serial.h>
 
+unsigned char read_register(unsigned char dest) {
+    __asm__ volatile("cli");
+    outb8(RTC_COMMAND, dest);
+    return inb8(RTC_DATA);
+    __asm__ volatile("sti");
+}
--- src/system/drivers/rtc.h
+++ src/system/drivers/rtc.h
@@ -5,6 +5,6 @@
 #define RTC_DATA    0x71
 #define RTC_STATUS  0x0B
 
-
+unsigned char read_register(unsigned char dest);
 
 #endif // __RTC_H__
