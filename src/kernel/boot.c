8bdb653e73363bc7baba573f61379199b2d61f83
Kevin Alavik <kevin@alavik.se>
2024-01-16 16:58:36 +0100

    broken things and me sad :(

--- src/kernel/boot.c
+++ src/kernel/boot.c
@@ -1,57 +1,53 @@
 #include <kernel/boot.h>
 #include <limine.h>
 #include <nighterm/nighterm.h>
-#include <system/idt/idt.h>
-#include <system/cpu/cpu.h>
-#include <system/memory/memory.h>
-#include <system/logger/sys_log.h>
-#include <system/utilities/utilities.h>
 #include <printf.h>
+#include <system/cpu/cpu.h>
+#include <system/idt/idt.h>
+#include <system/logger/sys_log.h>
+#include <system/memory/memory.h>
+#include <system/utilities/utilities.h>
 
 static volatile struct limine_module_request mod_request = {
-    .id = LIMINE_MODULE_REQUEST,
-    .revision = 0};
+    .id = LIMINE_MODULE_REQUEST, .revision = 0};
 
 static volatile struct limine_framebuffer_request framebuffer_request = {
-    .id = LIMINE_FRAMEBUFFER_REQUEST,
-    .revision = 1};
+    .id = LIMINE_FRAMEBUFFER_REQUEST, .revision = 1};
 
-void init_boot(int debug_info) 
-{
-    static struct limine_framebuffer *framebuffer;
+void init_boot(int debug_info) {
+  static struct limine_framebuffer *framebuffer;
 
-    struct limine_file *font = mod_request.response->modules[0];
-    framebuffer = framebuffer_request.response->framebuffers[0];
-    
-    int nstatus = nighterm_initialize(font->address,
-                                      framebuffer->address,
-                                      framebuffer->width,
-                                      framebuffer->height,
-                                      framebuffer->pitch,
-                                      framebuffer->bpp,
-                                      malloc);
+  struct limine_file *font = mod_request.response->modules[0];
+  framebuffer = framebuffer_request.response->framebuffers[0];
 
-    if(!nstatus) { 
-        dprintf("Nighterm failed to initialize, got status: %s", get_nighterm_return_string(nstatus));
-        hcf();    
-    }
-    log(OK, "Initiaized Nighterm");
-    init_idt();
-    log(OK, "Initiaized IDT");
-    init_physical_memory();
-    log(OK, "Initiaized PMM");
-    //nighterm_clear();
-    printf("\n");
-    printf("Welcome to ");
-    printfColor(77, 166, 255, 0, 0, 0, "Paradox\n");
-    char* nreturn =  get_nighterm_return_string(nstatus);
+  int nstatus = nighterm_initialize(
+      font->address, framebuffer->address, framebuffer->width,
+      framebuffer->height, framebuffer->pitch, framebuffer->bpp, malloc);
 
-    if(debug_info) {
-        printf("Debug Info:\n - %hhu module(s) loaded\n - Nighterm return code: %s\n", mod_request.response->module_count, nreturn);
-    }   
+  if (!nstatus) {
+    dprintf("Nighterm failed to initialize, got status: %s",
+            get_nighterm_return_string(nstatus));
+    hcf();
+  }
+  log(OK, "Initiaized Nighterm");
+  init_idt();
+  log(OK, "Initiaized IDT");
+  init_physical_memory();
+  log(OK, "Initiaized PMM");
+  // nighterm_clear();
+  printf("\n");
+  printf("Welcome to ");
+  printfColor(77, 166, 255, 0, 0, 0, "Paradox\n");
+  char *nreturn = get_nighterm_return_string(nstatus);
 
-    for(int i = 0; i < mod_request.response->module_count; i++) {
-        printf("%s: %u\n", mod_request.response->modules[i]->path, mod_request.response->modules[i]->size);
-    }
-    
+  if (debug_info) {
+    printf(
+        "Debug Info:\n - %hhu module(s) loaded\n - Nighterm return code: %s\n",
+        mod_request.response->module_count, nreturn);
+  }
+
+  for (int i = 0; i < mod_request.response->module_count; i++) {
+    printf("%s: %u\n", mod_request.response->modules[i]->path,
+           mod_request.response->modules[i]->size);
+  }
 }
