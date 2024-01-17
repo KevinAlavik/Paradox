adf26635236da263187550877dde07083d2f7bf3
puffer <kevin@alavik.se>
2024-01-17 18:05:27 +0100

    Create tar.h

---
+++ src/filesystem/tar.h
@@ -0,0 +1,22 @@
+#ifndef TAR_H_
+#define TAR_H_
+
+
+struct tar_header
+{
+    char filename[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char chksum[8];
+    char typeflag[1];
+};
+
+struct tar_header *headers[32];
+
+unsigned int getsize(const char *in);
+unsigned int parse(unsigned int address);
+
+#endif // TAR_H_
