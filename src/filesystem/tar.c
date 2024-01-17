f79249cec14f69266e0e45346a3cf76b9c87b837
puffer <kevin@alavik.se>
2024-01-17 18:06:07 +0100

    Create tar.c

---
+++ src/filesystem/tar.c
@@ -0,0 +1,43 @@
+#include "tar.h"
+
+unsigned int getsize(const char *in)
+{
+ 
+    unsigned int size = 0;
+    unsigned int j;
+    unsigned int count = 1;
+ 
+    for (j = 11; j > 0; j--, count *= 8)
+        size += ((in[j - 1] - '0') * count);
+ 
+    return size;
+ 
+}
+
+unsigned int parse(unsigned int address)
+{
+ 
+    unsigned int i;
+ 
+    for (i = 0; ; i++)
+    {
+ 
+        struct tar_header *header = (struct tar_header *)address;
+ 
+        if (header->name[0] == '\0')
+            break;
+ 
+        unsigned int size = getsize(header->size);
+ 
+        headers[i] = header;
+ 
+        address += ((size / 512) + 1) * 512;
+ 
+        if (size % 512)
+            address += 512;
+ 
+    }
+ 
+    return i;
+ 
+}
