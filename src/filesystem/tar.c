1ea01cea1159d014fe264166c382bf9dc924ce1d
puffer <kevin@alavik.se>
2024-01-18 08:10:26 +0100

    Update tar.c

--- src/filesystem/tar.c
+++ src/filesystem/tar.c
@@ -1,45 +1,72 @@
 #include "tar.h"
+#include <printf.h>
+#include <system/memory/memory.h>
+#include <strings.h>
 
-struct tar_header *headers[32];
+unsigned int getsize(const char *in) {
+  unsigned int size = 0;
+  unsigned int count = 1;
 
-unsigned int getsize(const char *in)
-{
- 
-    unsigned int size = 0;
-    unsigned int j;
-    unsigned int count = 1;
- 
-    for (j = 11; j > 0; j--, count *= 8)
-        size += ((in[j - 1] - '0') * count);
- 
-    return size;
- 
+  for (int j = 11; j > 0; j--, count *= 8)
+    size += ((in[j - 1] - '0') * count);
+
+  return size;
 }
 
-unsigned int parse_tar(unsigned int address)
-{
- 
-    unsigned int i;
- 
-    for (i = 0; ; i++)
-    {
- 
-        struct tar_header *header = (struct tar_header *)address;
- 
-        if (header->filename[i] == '\0')
-            break;
- 
-        unsigned int size = getsize(header->size);
- 
-        headers[i] = header;
- 
-        address += ((size / 512) + 1) * 512;
- 
-        if (size % 512)
-            address += 512;
- 
+void extractTarData(const char *rawData, unsigned int dataSize, struct Tar *tar) {
+  tar->files = NULL;
+  tar->fileCount = 0;
+
+  for (unsigned int offset = 0; offset < dataSize;) {
+    struct TarHeader *header = (struct TarHeader *)(rawData + offset);
+
+    if (header->filename[0] == '\0') {
+      break;
     }
- 
-    return i;
- 
+
+    struct File file;
+    file.size = getsize(header->size);
+    file.name = strdup(header->filename);
+    file.isDirectory = header->typeflag[0] == '5';
+
+    if (!file.isDirectory) {
+      file.content = (char *)malloc(file.size + 1);
+      memcpy(file.content, rawData + offset + 512, file.size);
+      file.content[file.size] = '\0';
+    } else {
+      file.content = NULL;
+    }
+
+    tar->files =
+        realloc(tar->files, (tar->fileCount + 1) * sizeof(struct File));
+    tar->files[tar->fileCount] = file;
+    tar->fileCount++;
+
+    offset += ((file.size + 511) / 512 + 1) * 512;
+  }
+}
+
+void printFileContent(const struct File *file) {
+  printf("Name: %s\n", file->name);
+  printf("Type: %s\n", file->isDirectory ? "Directory" : "File");
+  printf("Size: %u bytes\n", file->size);
+  if (!file->isDirectory) {
+    printf("Content: %s\n\n", file->content);
+  } else {
+    printf("Content: (Not available for directories)\n\n");
+  }
+}
+
+void printTarContents(const struct Tar *tar) {
+  for (unsigned int i = 0; i < tar->fileCount; ++i) {
+    printFileContent(&tar->files[i]);
+  }
+}
+
+void freeTar(struct Tar *tar) {
+  for (unsigned int i = 0; i < tar->fileCount; ++i) {
+    free(tar->files[i].name);
+    free(tar->files[i].content);
+  }
+  free(tar->files);
 }
