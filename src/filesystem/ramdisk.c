49b54d2531b5e20dd65e513af6a13c9d9c9368d4
puffer <kevin@alavik.se>
2024-01-18 09:26:14 +0000

    Fixed ramdisk

--- src/filesystem/ramdisk.c
+++ src/filesystem/ramdisk.c
@@ -7,7 +7,7 @@ int init_ramdisk(struct limine_file *tarfile) {
   extractTarData((const char *)tarfile->address, tarfile->size, &tar);
   ramdisk->tar = &tar;
   
-  if(ramdisk->tar->fileCount =< 0) {
+  if(ramdisk->tar->fileCount <= 0) {
     free(ramdisk->tar);
     freeTar(&tar);
     return 1;
