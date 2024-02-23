c91b22b4ac24ffe699ac49fe615af52969f79be8
puffer <kevin@alavik.se>
2024-02-23 10:01:36 +0100

    Update vfs.h

--- src/filesystem/vfs.h
+++ src/filesystem/vfs.h
@@ -1,4 +1,39 @@
 #ifndef __VFS_H__
 #define __VFS_H__
 
+// VFS Driver for Paradox
+
+#include <stdint.h>
+
+typedef struct {
+  VFS_t* disk;
+  char* label;
+} mount_point_t;
+
+typedef struct {
+  uint64_t address;
+  uint64_t id;
+  mount_point_t mount_point;
+  
+  uint64_t size;
+  uint64_t actual_size;
+} drive_t;
+
+typedef struct {
+  drive_t* drives;
+
+  uint64_t address;
+  uint64_t id;
+  uint64_t size;
+  uint64_t actual_size;
+} VFS_t;
+
+VFS_t* init_vfs();
+
+int mount_drive(drive_t drive);
+int unmount_drive(uint64_t id);
+
+void* read_disk(uint64_t id);
+void write_disk(uint64_t id, void* data);
+
 #endif // __VFS_H_
