3cf36bd19f765ce7b7f6bcaa6041b8ba463f9823
puffer <kevin@alavik.se>
2024-02-24 18:34:58 +0100

    Update tga.h

---
+++ src/corelib/tga.h
@@ -0,0 +1,16 @@
+#ifndef __TGA_H__
+#define __TGA_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+unsigned int *tga_parse(unsigned char *ptr, int size);
+
+void draw_targa_image(char *image, int x, int y);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TGA_H__ */
