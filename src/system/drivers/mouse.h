f67f844ed9e3c38b74d937e4e992a964757db981
puffer <kevin@alavik.se>
2024-02-24 20:52:14 +0100

    Started on mouse driver

---
+++ src/system/drivers/mouse.h
@@ -0,0 +1,21 @@
+#ifndef __MOUSE_H__
+#define __MOUSE_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <vga.h>
+#include <system/idt/idt.h>
+#include <serial/serial.h>
+
+extern uint32_t mouse_x;
+extern uint32_t mouse_y;
+
+extern bool mouse_left_pressed;
+extern bool mouse_right_pressed;
+
+extern bool mouse_moved;
+
+void mouse_init();
+
+#endif // __MOUSE_H__
