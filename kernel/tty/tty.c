c98686bea63ce8025e49af09169987862b60878b
puffer <kevin@alavik.se>
2024-03-24 20:52:59 +0100

    Update tty.c

--- kernel/tty/tty.c
+++ kernel/tty/tty.c
@@ -11,6 +11,20 @@ int tty_spawn(uint8_t id)
     return 1;
 
   tty temp;
+  struct nighterm_ctx context;
+  int s = nighterm_initialize(&context,
+            NULL,
+            framebuffer->address,
+            framebuffer->width,
+            framebuffer->height,
+            framebuffer->pitch,
+            framebuffer->bpp,
+            malloc, free);
+  if(s != NIGHTERM_OK)
+    return 2;
 
-  
+  temp.id = id;
+  temp.context = &context;
+  ttys[id] = &temp;
+  return 0;
 }
